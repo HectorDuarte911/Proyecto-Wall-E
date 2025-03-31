d434974efb41ddf295fa89e582b4cc0bc2e7d314
HectorDuarte911 <hduartevazquez@gmail.com>
2025-03-31 11:06:48 +0800

    Add Spectre.Console

--- obj/Debug/net8.0/Proyecto-Wall-E.AssemblyInfo.cs
+++ obj/Debug/net8.0/Proyecto-Wall-E.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto-Wall-E")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b2bd9ca4dc8c222caff22701bd7d3e9476d710")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto-Wall-E")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto-Wall-E")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
